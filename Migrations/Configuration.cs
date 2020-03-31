34940e9d95bd5aafab5002b5f579ca3dffc51adb
LukaszKwecko <kweckolukasz@gmail.com>
2020-03-31 12:36:20 +0200

    migrations enabled

--- Migrations/Configuration.cs
+++ Migrations/Configuration.cs
@@ -5,14 +5,14 @@
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Vidly.App_Start.MyDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<Vidly.App_Start.ApplicationContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(Vidly.App_Start.MyDbContext context)
+        protected override void Seed(Vidly.App_Start.ApplicationContext context)
         {
             //  This method will be called after migrating to the latest version.
 
