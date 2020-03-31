014bcaf6eaffb698e25bfc3160e564359018d8d0
LukaszKwecko <kweckolukasz@gmail.com>
2020-03-31 12:28:17 +0200

    initial

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Vidly.App_Start.MyDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Vidly.App_Start.MyDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
