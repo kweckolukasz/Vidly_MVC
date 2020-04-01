31170e2a7d1a3f7a946c514724c28b6b0ebfdb85
LukaszKwecko <kweckolukasz@gmail.com>
2020-04-01 10:14:48 +0200

    Customers and Movies added to DB

--- App_Start/ApplicationContext.cs
+++ App_Start/ApplicationContext.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using Vidly.Models;
 
 namespace Vidly.App_Start
 {
     public class ApplicationContext : DbContext
     {
+        DbSet<Customer> Customers { get; set; }
+        DbSet<Movie> Movies { get; set; }
     }
 }
--- Migrations/202003311037263_InitialModel.cs
+++ Migrations/202003311037263_InitialModel.cs
@@ -7,10 +7,30 @@
     {
         public override void Up()
         {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        name = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.Movies",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        name = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
         }
         
         public override void Down()
         {
+            DropTable("dbo.Movies");
+            DropTable("dbo.Customers");
         }
     }
 }
